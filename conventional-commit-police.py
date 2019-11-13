4861a7175644c5f4878de62985f93ad6641b47fe
Janos Molnar <janosmolnar1001@gmail.com>
2019-11-13 21:10:18 +0100

    feat: Add script to check conventional-commit rules

---
+++ conventional-commit-police.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys, re
+
+
+regex = "(feat|fix|docs|style|refactor|test|chore)(.*):(.+)"
+
+commit_message_file = open(sys.argv[1]) #The first argument is the file
+commit_message = commit_message_file.read().strip()
+
+if re.search(regex, commit_message) is None:
+    print("""
+        It's the Conventional Commit police!!!
+        You are under arrest!
+        Please follow the rules: https://www.conventionalcommits.org/en/v1.0.0/
+    """)
+    sys.exit(1)
+
+print("Inspection finished, you are free to go now.")
+sys.exit(0)
